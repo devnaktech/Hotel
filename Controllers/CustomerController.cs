d41b9397f09a6315a4241aa5298662d12cdc01e4
SeavE2303 <chhoukseave@gmail.com>
2025-03-18 20:52:02 +0700

    updated customer-seavE

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Midterm.Data;
+using Midterm.Models;
+
+namespace Midterm.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly AppDbContext _context;
+        public CustomerController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var cust = _context.Customers.ToList();
+            return View(cust);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Customer cust)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Customers.Add(cust);
+                int insert = _context.SaveChanges();
+                return insert > 0 ? RedirectToAction(nameof(Index)) : View();
+            }
+            {
+                ModelState.AddModelError("", "Failed to save the customer. Please try again");
+            }
+            return View(cust);
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var cust = _context.Customers.Find(id);
+            if (cust == null)
+            {
+                return NotFound();
+            }
+            return PartialView("_Edit", cust);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Customer cust)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Customers.Update(cust);
+                int update = _context.SaveChanges();
+                return update > 0 ? RedirectToAction(nameof(Index)) : View();
+
+            }
+            {
+                ModelState.AddModelError("", "Failed to save the customer. Please try again");
+            }
+            return View(cust);
+        }
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            var cust = _context.Customers.Find(id);
+            if (cust == null)
+            {
+                return NotFound();
+            }
+            return PartialView("_Detail", cust);
+        }
+        public IActionResult Delete(int id)
+        {
+            var cust = _context.Customers.Find(id);
+            if (cust == null)
+            {
+                return NotFound();
+            }
+            _context.Customers.Remove(cust);
+            int delete = _context.SaveChanges();
+            return delete > 0 ? RedirectToAction(nameof(Index)) : View();
+        }
+    }
+}
--- obj/Release/net8.0/Midterm.AssemblyInfo.cs
+++ obj/Release/net8.0/Midterm.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("5b06965e-063c-4cd2-9691-1b3d25c8a527")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Midterm")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1785adfa9bbe3fd4bdeb59fc4342bd6f2c63c8d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Midterm")]
